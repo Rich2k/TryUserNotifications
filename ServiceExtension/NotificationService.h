128381d283bdd4ae593279e0bfd8fadfa3a0344e
Andrew Tokarev <git@atkit.com>
2016-08-06 13:48:19 +0200

    Added service extension

---
+++ ServiceExtension/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  ServiceExtension
+//
+//  Created by Andrew on 06/08/16.
+//  Copyright © 2016 Andrey Tokarev. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
---
+++ ServiceExtension/NotificationService.m
@@ -0,0 +1,36 @@
+//
+//  NotificationService.m
+//  ServiceExtension
+//
+//  Created by Andrew on 06/08/16.
+//  Copyright © 2016 Andrey Tokarev. All rights reserved.
+//
+
+#import "NotificationService.h"
+
+@interface NotificationService ()
+
+@property (nonatomic, strong) void (^contentHandler)(UNNotificationContent *contentToDeliver);
+@property (nonatomic, strong) UNMutableNotificationContent *bestAttemptContent;
+
+@end
+
+@implementation NotificationService
+
+- (void)didReceiveNotificationRequest:(UNNotificationRequest *)request withContentHandler:(void (^)(UNNotificationContent * _Nonnull))contentHandler {
+    self.contentHandler = contentHandler;
+    self.bestAttemptContent = [request.content mutableCopy];
+    
+    // Modify the notification content here...
+    self.bestAttemptContent.title = [NSString stringWithFormat:@"%@ [modified]", self.bestAttemptContent.title];
+    
+    self.contentHandler(self.bestAttemptContent);
+}
+
+- (void)serviceExtensionTimeWillExpire {
+    // Called just before the extension will be terminated by the system.
+    // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+    self.contentHandler(self.bestAttemptContent);
+}
+
+@end
